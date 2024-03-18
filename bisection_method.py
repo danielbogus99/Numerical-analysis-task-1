f0f4de0e282c8590df3335a24a3b580c98ecc29b
danielbogus99 <danik444@walla.co.il>
2024-03-18 13:12:52 +0200

    fixed

--- bisection_method.py
+++ bisection_method.py
@@ -1,23 +1,28 @@
 import math
 import numpy as np
-from colors import bcolors
+import sympy as sp
+
+
 
 def max_steps(a, b, err):
     s = int(np.floor(- np.log2(err / (b - a)) / np.log2(2) - 1))
     return s
 
+
+
+
 def bisection_method(f, a, b, tol=1e-6):
-    if np.sign(f(a)) == np.sign(f(b)):
-        raise Exception("The scalars a and b do not bound a root")
+
     c, k = 0, 0
     steps = max_steps(a, b, tol)
 
     print("{:<10} {:<15} {:<15} {:<15} {:<15} {:<15} {:<15}".format("Iteration", "a", "b", "f(a)", "f(b)", "c", "f(c)"))
 
-    while abs(b - a) >= tol and k < steps:  # Adjusted stopping condition
-        c = a + (b - a) / 2
 
-        if f(c) == 0 :
+    while abs(b - a) > tol and k <= steps:
+        c = (a + b) / 2
+
+        if f(c) == 0:
             return c
 
         if f(c) * f(a) < 0:
@@ -26,21 +31,32 @@ def bisection_method(f, a, b, tol=1e-6):
             a = c
 
         print("{:<10} {:<15.6f} {:<15.6f} {:<15.6f} {:<15.6f} {:<15.6f} {:<15.6f}".format(k, a, b, f(a), f(b), c, f(c)))
-        if round(a) == b:
-            return "not found"
         k += 1
-    
+
     return c
 
-if __name__ == '__main__':
-    # Git:https://github.com/danielbogus99/Numerical-analysis-task-1
-    # Date: 18.03.2024
-    # Group: Eytan Stryzhack 336244959,
-    # Daniel Boguslavsky 207915729
-    # , Shifra Avigdor 207067125,
-    # David Moalem 203387337
+def find_all_roots(f, a, b, tol=1e-6):
+    roots = []
+    x = np.linspace(a, b, 1000)
 
-    # Name: Daniel Boguslavsky
-    f = lambda x: x**2 - 4*x
-    roots = bisection_method(f, -1, 0.5)
-    print(bcolors.OKBLUE, f"\nThe equation f(x) has an approximate root at x = {roots}", bcolors.ENDC)
+    x_sym = sp.symbols('x')
+    f_sym = f(x_sym)
+
+    for i in range(len(x) - 1):
+        if np.sign(f(x[i])) != np.sign(f(x[i + 1])):
+            interval = sp.Interval(x[i], x[i + 1])
+            try:
+                root = sp.nsolve(f_sym, (interval.start + interval.end)/2, tol=tol)
+                roots.append(root)
+            except ValueError:
+                print(f"Could not find root within tolerance in interval {interval}")
+
+    return roots
+
+if __name__ == '__main__':
+    f = lambda x: x**2-4
+    start = -4
+    end = 3
+
+    roots = find_all_roots(f, start, end)
+    print(roots)
